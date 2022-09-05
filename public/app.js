7de5a3d694d10ff8d46bf9bddeb6a814dcc753b6
axelhagerback <axel.hagerback@gmail.com>
2022-09-05 12:38:40 +0200

    fick uppgift A att fungera

---
+++ public/app.js
@@ -0,0 +1,7 @@
+console.log("HELLO JAVASCRIPT");
+
+function myFunction() {
+    var x = document.getElementById("myTextarea").value;
+    document.getElementById("demo").innerHTML = "Hej, " + x;
+    
+};
